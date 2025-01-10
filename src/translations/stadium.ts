2cc5b6bbf189d530c4affce4fe72b8428dd87d84
Felipe Osorio <felipe@stadiumcompete.com>
2025-01-10 16:13:08 -0500

    [ES] Updated spanish-language phrases for general & stadium ts files

--- src/translations/stadium.ts
+++ src/translations/stadium.ts
@@ -8,16 +8,20 @@ export const T_STADIUM = {
   },
   STADIUM_IS_NOW_FREE: {
     [LANGUAGE.EN]: "STADIUM is now 100% free for organizers.",
+    [LANGUAGE.ES]: "STADIUM es 100% gratis para los organizadores.",
     [LANGUAGE.FR]: "STADIUM est 100% gratuit pour les organisateurs.",
   },
   STADIUM_HELP_US_KEEP_IT_FREE: {
     [LANGUAGE.EN]:
       "Help us keep it free - and get extra perks - with a monthly subscription (starting at just $2).",
+    [LANGUAGE.ES]:
+      "Ayudanos a mantenerlo gratis - y recibe extras - con una suscripción mensual (a partir de solo $2 al mes).",
     [LANGUAGE.FR]:
       "Aidez-nous à le maintenir gratuit - et obtenez des avantages - avec un abonnement mensuel (à partir de seulement 2$).",
   },
   STADIUM_SUBSCRIBE_TO_SUPPORT: {
     [LANGUAGE.EN]: "Subscribe to Support STADIUM",
+    [LANGUAGE.ES]: "Suscríbete a STADIUM",
     [LANGUAGE.FR]: "Soutenez STADIUM",
   },
 };
